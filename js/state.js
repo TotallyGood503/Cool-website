37000cfca8d025928880b6404a81523632189e80
TotallyGood503 <kolaba.kolaba@gmail.com>
2024-08-13 17:04:07 +0300

    Add files via upload

---
+++ js/state.js
@@ -0,0 +1,68 @@
+const state = {
+    header: [
+        { url: 'telegram.svg', text: 'Телеграм' },
+        { url: 'viber.svg', text: 'Вайбер(нема)' }
+    ],
+    section1: [
+        { text: 'Код на джаву', url: 'java-svgrepo-com.svg' },
+        { text: 'Код на пайтон', url: 'python-svgrepo-com.svg' },
+        { text: 'Код на КСС', url: 'css-3.svg' },
+        { text: 'Код на Луа', url: 'lua-svgrepo-com.svg' },
+        { text: 'Код на С#', url: 'c--4.svg' },
+        { text: 'Код на Джаваскріпт', url: 'java-svgrepo-com (1).svg' }
+    ],
+    section3: [
+        { text: 'Код не важкий', paragraph: 'на HTML та КСС працювати це як яблуко зїсти' },
+        { text: 'Классний результат', paragraph: 'вийде гарно і круто :)' },
+        { text: 'Не довго часу на день', paragraph: 'можна по менше 1 години на день і все бомба' },
+        { text: 'Можлива крутість', paragraph: 'якщо старатись, ти крутеликом станеш' },
+        { text: 'менше 2 ГБ на компі', paragraph: 'багато памяті не треба' },
+        { text: 'ВЕСЕЛОООО', paragraph: 'можна щось робити якщо скучно' }
+    ],
+    section4: [
+        { url: 'java-svgrepo-com.svg' },
+        { url: 'python-svgrepo-com.svg' },
+        { url: 'html-1.svg' },
+        { url: 'css-3.svg' },
+        { url: 'lua-svgrepo-com.svg' }
+    ],
+    section5: [
+        { li: 'Це картинка того,' },
+        { li: 'як ти можеш виглядати після кодингу...' },
+        { li: 'або не так' },
+
+        { li: 'Тут мають бути причини чому треба кодити' },
+        { li: 'Але їх поки що нема' },
+        { li: 'не хвилюйся,' },
+        { li: 'скоро будуть ;)' }
+    ],
+    section6: [
+        { text: '7', paragraph: 'мов програмування на вивчення' },
+        { text: '1', paragraph: 'місяць на одну мову' },
+        { text: '2', paragraph: 'години на день' }
+    ],
+    section7: [
+        { url: 'python-svgrepo-com.svg' },
+        { url: 'java-svgrepo-com.svg' },
+        { url: 'htl-1.svg' }
+    ],
+    footer: [
+        { href: '#', li: 'Крутий сайт' },
+        { href: '#', li: 'Картинка борща' },
+        { href: '#', li: 'Чийсь ютуб' },
+        { href: '#', li: 'Код на Java' },
+
+        { text: 'Крутий сайт' },
+        { text: 'Сам робив' },
+        { text: 'оцени 10/10' },
+
+        { url: 'map.svg', span: '(локація автора тут)' },
+        { url: 'message.svg', span: 'недампочту@gmail.com' },
+        { url: 'tel.svg', span: 'не дам телефон' },
+
+        { href: '#', url: 'youtube.svg' },
+        { href: '#', url: 'telegram.svg' }
+    ]
+
+}
+
